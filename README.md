93f1561e97230c9ba593e8e0f9e481bca299fdfd
Priyank Khunt <webturtles18@gmail.com>
2023-07-22 11:06:07 +0530

    Read me updated

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# quasar-poc
+# Quasar-poc
+
 Quasar Project for Proof of Concept
+
+- Todo sample application
+
+## Installation
+
+```
+npm install
+# or
+yarn install
+```
+
+## Build and Run
+
+```
+npm run dev
+# or
+yarn dev
+```
+
+build
+
+```
+npm build
+# or
+yarn build
+```
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -79,53 +79,6 @@ const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 };
 
-const essentialLinks = computed(() => {
-  return [
-    {
-      title: "Docs",
-      caption: "quasar.dev",
-      icon: "school",
-      link: "https://quasar.dev",
-    },
-    {
-      title: "Github",
-      caption: "github.com/quasarframework",
-      icon: "code",
-      link: "https://github.com/quasarframework",
-    },
-    {
-      title: "Discord Chat Channel",
-      caption: "chat.quasar.dev",
-      icon: "chat",
-      link: "https://chat.quasar.dev",
-    },
-    {
-      title: "Forum",
-      caption: "forum.quasar.dev",
-      icon: "record_voice_over",
-      link: "https://forum.quasar.dev",
-    },
-    {
-      title: "Twitter",
-      caption: "@quasarframework",
-      icon: "rss_feed",
-      link: "https://twitter.quasar.dev",
-    },
-    {
-      title: "Facebook",
-      caption: "@QuasarFramework",
-      icon: "public",
-      link: "https://facebook.quasar.dev",
-    },
-    {
-      title: "Quasar Awesome",
-      caption: "Community Quasar projects",
-      icon: "favorite",
-      link: "https://awesome.quasar.dev",
-    },
-  ];
-});
-
 const todayDate = computed(() => {
   const timeStamp = Date.now();
   return date.formatDate(timeStamp, "dddd D MMMM");
